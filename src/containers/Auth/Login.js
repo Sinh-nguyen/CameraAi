9a236d928ff97a721e96d5ac25cd8efcb456a063
Sinh Nguyen <sinhnguyen@MacBook-Pro-cua-Sinh.local>
2021-08-11 09:01:15 +0700

    first

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -7,9 +7,32 @@ import "./Login.scss";
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.btnLogin = React.createRef();
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword:false,
+    };
   }
 
+  handleOnChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  handleOnChangePassword = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  handleShowHidePassword=()=>{
+    this.setState({
+      isShowPassword:!this.state.isShowPassword
+    })
+  }
+
+  handleLogin = () => {};
   render() {
     return (
       <div className="login-background">
@@ -22,23 +45,40 @@ class Login extends Component {
                 type="text"
                 className="form-control"
                 placeholder="Enter your name"
+                value={this.state.username}
+                onChange={(event) => this.handleOnChangeUsername(event)}
               />
             </div>
-            <div className="col-12 form-group">
+            <div className="col-12 form-group login-input">
               <label>Password</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your password"
-              />
+              <div className="custom-input-password">
+                <input
+                  type={this.state.isShowPassword?'text':'password'}
+                  className="form-control"
+                  placeholder="Enter your password"
+                  onChange={(event) => this.handleOnChangePassword(event)}
+                />
+                <span 
+                  onClick={()=>this.handleShowHidePassword()}
+                >
+                  <i class={this.state.isShowPassword?"far fa-eye":"far fa-eye-slash"}></i>
+                </span>
+              </div>
             </div>
             <div className="col-12">
-              <button className="btn-login">Login</button>
+              <button
+                className="btn-login"
+                onClick={() => {
+                  this.handleLogin();
+                }}
+              >
+                Login
+              </button>
             </div>
             <div className="col-12">
               <span>Forgot your password</span>
             </div>
-            <div className="col-12 text-center">
+            <div className="col-12 text-center mt-3">
               <span className="text-other-login">Or Login with:</span>
             </div>
             <div className="col-12 social-lgi">
